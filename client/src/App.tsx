def3ffd7346c378192ef67f1ce659333e599c0d2
Marcio Junior <mps.junior@hotmail.com>
2026-08-12 16:38:37 -0300

    style(client): padroniza formatação do App

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,10 @@
-import { Routes, Route } from 'react-router-dom';
-import { HomePage } from './pages/HomePage';
+import { Route, Routes } from "react-router-dom";
+import { HomePage } from "./pages/HomePage";
 
 export function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-    </Routes>
-  );
+    return (
+        <Routes>
+            <Route path="/" element={<HomePage />} />
+        </Routes>
+    );
 }
