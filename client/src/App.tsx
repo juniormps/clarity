4cdb46b25b962492eff19f1306e770aa35fe0c40
Marcio Junior <mps.junior@hotmail.com>
2026-08-18 15:15:29 -0300

    refactor(client): converte componente App para export default
    - ajusta a exportação do App para o padrão default
    - atualiza o import em main.tsx de acordo

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 
-export function App() {
+function App() {
     return (
         <Routes>
             <Route path="/" element={<HomePage />} />
         </Routes>
     );
 }
+
+export default App;
